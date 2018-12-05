2c26599a5608a04057f98997a7504113f200c7c7
Cezar <c_augusto.rocha@hotmail.com>
2018-12-05 19:49:46 -0200

    Initial commit

---
+++ src/main/java/server/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package server.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("Ola mundo");
+		
+		return "home/index";
+	}
+}
